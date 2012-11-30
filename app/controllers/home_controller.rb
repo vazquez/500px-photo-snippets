520be436855470e4ab9369ac4b864936f58050fb
Arthur Neves <arthurnn@gmail.com>
2012-11-30 16:55:30 -0500

    better

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -1,16 +1,24 @@
 class HomeController < ApplicationController
-  def index
-    redirect_to '/oauth/login' and return if session[:token].blank?
-    consumer = OAuth::Consumer.new(AuthenticationController::CONSUMER_KEY, AuthenticationController::CONSUMER_SECRET, {:site=> AuthenticationController::BASE_URL})
-    access_token = OAuth::AccessToken.new(consumer, session[:token], session[:secret])
-    @fav_photos = JSON.parse(access_token.get('/photos?feature=popular&rpp=50&page=1&image_size=3&only=People').body)['photos']
+  before_filter :get_client
 
-    @user = JSON.parse(access_token.get('/users').body)['user']
+  def get_client
+    return if session[:token].blank?
+    consumer = OAuth::Consumer.new(AuthenticationController::CONSUMER_KEY, AuthenticationController::CONSUMER_SECRET, {:site=> AuthenticationController::BASE_URL})
+    @client = OAuth::AccessToken.new(consumer, session[:token], session[:secret])
+  end
+
+  def get_api url
+    JSON.parse(@client.get(url).body)
+  end
+
+  def index
+    redirect_to '/oauth/login' and return if @client.nil?
+    @fav_photos = get_api('/photos?feature=popular&rpp=50&page=1&image_size=3&only=People')['photos']
+    @user = get_api('/users')['user']
     session[:username] = @user['username']
   end
   def photo_selected
     @selected_ids = params['data']
-    p @selected_ids.inspect
 
     @s = Snippet.create! :photos => @selected_ids, :username => session[:username]
     
