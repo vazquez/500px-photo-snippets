2bd2edd73325aa109e4e40a965cc5417b58a52fb
Arthur Neves <arthurnn@gmail.com>
2012-11-30 16:15:03 -0500

    fixing login

--- app/controllers/authentication_controller.rb
+++ app/controllers/authentication_controller.rb
@@ -1,19 +1,27 @@
-require 'oauth'
 class AuthenticationController < ApplicationController
-	CONSUMER_KEY = 'HNF9wZddjSvtaDa4CZ1Yxf4Vs1lQCF7Ll3B313aO'
+  CONSUMER_KEY = 'HNF9wZddjSvtaDa4CZ1Yxf4Vs1lQCF7Ll3B313aO'
   CONSUMER_SECRET = 'DFnGdWD0mtO4SexCejBjdVmmSEgJ9rLRugAM2KUS'
   BASE_URL = 'https://api.500px.com/v1'
 
-	def oauth_login
-		consumer = OAuth::Consumer.new(CONSUMER_KEY, CONSUMER_SECRET, {:site=> BASE_URL})
-  	request_token = consumer.get_request_token
-  	redirect_to request_token.authorize_url 
-	end
-
-	def oauth_callback
-		consumer = OAuth::Consumer.new(CONSUMER_KEY, CONSUMER_SECRET, {:site=> BASE_URL})
+  def index
+  end
+  
+  def login
+    consumer = OAuth::Consumer.new(CONSUMER_KEY, CONSUMER_SECRET, {:site=> BASE_URL})
     request_token = consumer.get_request_token
-    oauth_verifier = params['oauth_verifier']
-    redirect_to :controller=>'home', :action=>'index', :oauth_verifier=>oauth_verifier
-	end
+    #redirect_to request_token.authorize_url
+    access_token = consumer.get_access_token(request_token, {}, { :x_auth_mode => 'client_auth', :x_auth_username => params[:username], :x_auth_password => params[:password] })
+    
+    session[:token] = access_token.token
+    session[:secret] = access_token.secret
+    redirect_to :controller=>'home', :action=>'index'
+  rescue
+    puts 'didnt work'
+
+    flash[:notice] = "Wrong"
+    redirect_to '/oauth/login'
+    
+  end
+  
+  
 end
--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -1,21 +1,23 @@
-require 'oauth'
-require 'json'
 class HomeController < ApplicationController
   def index
-  	# @fav_photos = params[:oauth_verifier]
-  	redirect_to '/oauth/login' if params[:oauth_verifier].blank?
-  	consumer = OAuth::Consumer.new(AuthenticationController::CONSUMER_KEY, AuthenticationController::CONSUMER_SECRET, {:site=> AuthenticationController::BASE_URL})
-  	access_token = OAuth::AccessToken.new(consumer, params[:oauth_verifier])
-  	@fav_photos = JSON.parse(access_token.get('/photos?feature=popular&rpp=50&page=1&image_size=3&only=People').body)['photos']
+    redirect_to '/oauth/login' and return if session[:token].blank?
+    consumer = OAuth::Consumer.new(AuthenticationController::CONSUMER_KEY, AuthenticationController::CONSUMER_SECRET, {:site=> AuthenticationController::BASE_URL})
+    access_token = OAuth::AccessToken.new(consumer, session[:token], session[:secret])
+    @fav_photos = JSON.parse(access_token.get('/photos?feature=popular&rpp=50&page=1&image_size=3&only=People').body)['photos']
+
+    b = access_token.get('/users').body
+    @user = JSON.parse(b)['user']
+    puts @user.inspect
+    
   end
   def photo_selected
-  	@selected_ids = params['data']
-  	p @selected_ids.inspect
-  	respond_to do |format|
-  		format.json do 
-  			render :json=>{:success=>true}
-  		end
-  	end
+    @selected_ids = params['data']
+    p @selected_ids.inspect
+    respond_to do |format|
+      format.json do 
+        render :json=>{:success=>true}
+      end
+    end
   end
   def show_snippet
 
