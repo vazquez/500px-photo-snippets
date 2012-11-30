9aa2c12f3745d9bd505b2527fac4b252e5bc3cbd
Arthur Neves <arthurnn@gmail.com>
2012-11-30 16:59:53 -0500

    adding 404

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -19,18 +19,18 @@ class HomeController < ApplicationController
   end
   def photo_selected
     @selected_ids = params['data']
-
     @s = Snippet.create! :photos => @selected_ids, :username => session[:username]
-    
     respond_to do |format|
       format.json do 
-        render :json=>{:success=>true, :url => "/s/#{@s._id}"}
+        render :json => {:success=>true, :url => "/s/#{@s._id}"}
       end
     end
   end
   def snippet
     oid = params[:id]
     @snippet = Snippet.find(oid)
-    
+    not_found if @snippet.blank?
+  rescue
+    not_found
   end
 end
