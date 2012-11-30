1767963b4c1be0720215c0808ca3b6470b89cd91
Arthur Neves <arthurnn@gmail.com>
2012-11-30 17:51:51 -0500

    fixing indentation

--- app/assets/javascripts/home.js
+++ app/assets/javascripts/home.js
@@ -2,35 +2,35 @@
 // All this logic will automatically be available in application.js.
 var selected_id = [];
 $(document).ready(function(){
-	$(".selectable-image").click(function(){
-		var img = $(this);
-		if (img.hasClass('selected')){
-			img.removeClass('selected');
-			selected_id.splice(selected_id.indexOf(this.id), 1);
-		}else{
-			img.addClass('selected');
-			selected_id.push(this.id);
-			if (selected_id.length >= 6){
-				//bam, go to the backend;
-				$(".selectable-image:not(.selected)").fadeOut(function(){
-					$(this).remove();
-				});
-				$(".selected").removeClass('selected');
-				$('#overlay').css('display', 'visible');
-				$( "#sortable" ).sortable();
-  			$( "#sortable" ).disableSelection();
-  			$("#sortable").addClass("show-order");
-			  // $.ajax({
-				 // type: 'POST',
-				 // url: '/selected',
-				 // data: {data:selected_id},
-				 // success: function(data){
-				 //     if (data.success){
-					//  window.location.href = data.url;
-				 //     }
-				 // },
-			  // });
-			}
-		}
-	})
+    $(".selectable-image").click(function(){
+	var img = $(this);
+	if (img.hasClass('selected')){
+	    img.removeClass('selected');
+	    selected_id.splice(selected_id.indexOf(this.id), 1);
+	}else{
+	    img.addClass('selected');
+	    selected_id.push(this.id);
+	    if (selected_id.length >= 6){
+		//bam, go to the backend;
+		$(".selectable-image:not(.selected)").fadeOut(function(){
+		    $(this).remove();
+		});
+		$(".selected").removeClass('selected');
+		$('#overlay').css('display', 'visible');
+		$( "#sortable" ).sortable();
+  		$( "#sortable" ).disableSelection();
+  		$("#sortable").addClass("show-order");
+		// $.ajax({
+		// type: 'POST',
+		// url: '/selected',
+		// data: {data:selected_id},
+		// success: function(data){
+		//     if (data.success){
+		//  window.location.href = data.url;
+		//     }
+		// },
+		// });
+	    }
+	}
+    })
 })
